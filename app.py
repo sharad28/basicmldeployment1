6b2e64d267b4f59861f58457d3c8055848ecd408
Sarad Mishra <saradmishra28@gmail.com>
2022-07-12 01:53:59 +0530

    apply changes in app.py

--- app.py
+++ app.py
@@ -15,16 +15,19 @@ app = Flask(__name__)
 def input():
     return flask.render_template('index.html')
 
-@app.route('/predict',methods=['post','get'])
+@app.route('/predict',methods=['POST'])
 def predict():
-    model = joblib.load('pipeline.pkl')
-    to_pred = requrest.form.to_dict()
-    to_pred = list(to_pred.values)
-    to_pred = np.array(list(map(float,to_pred))).reshape(1,-1)
-    print(to_pred)
-    y_bar = model.predict(to_pred)
-    return jsonify ({'prediction':list(y_bar)})
-    
+    lr = joblib.load('pipeline.pkl')
+    to_predict_list = request.form.to_dict()
+    to_predict_list = list(to_predict_list.values())
+    to_predict_list = np.array(list(map(float, to_predict_list))).reshape(1, -1)
+    print(to_predict_list)
+    prediction = lr.predict(to_predict_list)
+    print(prediction)
+    return jsonify({'prediction': list(prediction)})
+
+
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=8080)
-    app.run(port=8000)
+    app.run(host='0.0.0.0', port=8080)
+
+    
